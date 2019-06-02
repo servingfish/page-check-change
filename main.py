ddc53c070b4f3e910d821060bb25d00a26bad632
BuildTools <unconfigured@null.spigotmc.org>
2019-06-02 03:35:50 +0300

    Added getting content and compare content. IndexError on scripts.py, dunno what reason at all...

--- main.py
+++ main.py
@@ -4,14 +4,20 @@
 import os
 import scripts
 
+
 cont = True
 while cont:
     os.system('cls')
-    print("Welcome to website change checker by servingfish!\n[1]Enter a new URL")
+    print("Welcome to website change checker by servingfish!\n[1]Enter a new URL\n[2]Start tracking!\n[0]Exit")
     selec = input("Enter a selection: ")
 
     if selec == "1":
         scripts.newurl()
+    elif selec == "2":
+        scripts.track()
+    elif selec == "0":
+        cont = False
+
 
 # browser = mechanicalsoup.StatefulBrowser()
 # browser.open(url)
--- scripts.py
+++ scripts.py
@@ -1,12 +1,54 @@
 import os
+import urllib.request
 
 url = []
-
+name = []
+current_cont = []
+new_cont = []
+status = []
 
 def newurl():
     os.system('cls')
     print("New URL Screen\nHere are the current URLs")
-    for i in range(len(url)):
-        print(str(i + 1) + ". " + url[i])
+    for i in range(len(url)):  # Print the current url list
+        print(str(i + 1) + ". " + name[i] + "  " + url[i])  # "{Number}. {url}"
     inputurl = input("Enter a new URL: ")
+    inputname = input("Enter a name for this URL: ")
     url.append(inputurl)
+    name.append(inputname)
+
+
+def track():
+    os.system('cls')
+    if len(url) == "0":
+        print("There is no URL to track! Go back and add more.")
+        return
+    get_contents()  # Get current contents of URLs
+    cont = True
+    while cont:
+        print("Now tracking the URLs. They will be checked every 5 seconds.\n")
+        for i in range(len(url)):
+            compare_contents()
+            print(str(i + 1) + ". " + name[i] + status[i])
+
+
+def get_contents():
+    for i in range(len(url)):
+        print(url)
+        print(name)
+        print(i)
+        current_cont[i] = urllib.request.urlopen(url[i]).read()
+
+
+def compare_contents():
+    # Get new contents
+    for i in range(len(url)):
+        # TODO Fix the damn IndexError list assignment index out of range
+        new_cont[i] = urllib.request.urlopen(url[i]).read()
+
+    # Compare them and return status
+    for i in range(len(url)):
+        if current_cont[i] == new_cont[i]:
+            status[i] = "No change!"
+        else:
+            status[i] = "Changed!"
